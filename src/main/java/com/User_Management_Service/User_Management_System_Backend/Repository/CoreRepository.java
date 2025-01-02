0dd186fc5c1a781661907aa09755d08eb48d3c0e
suruthiii <suruthi0611@gmail.com>
2025-01-02 12:37:22 +0530

    fix: core repository

--- src/main/java/com/User_Management_Service/User_Management_System_Backend/Repository/CoreRepository.java
+++ src/main/java/com/User_Management_Service/User_Management_System_Backend/Repository/CoreRepository.java
@@ -7,3 +7,4 @@ import org.springframework.data.repository.NoRepositoryBean;
 @NoRepositoryBean
 public interface CoreRepository<T, ID> extends JpaRepository<T, ID>, JpaSpecificationExecutor<T> {
 }
+
